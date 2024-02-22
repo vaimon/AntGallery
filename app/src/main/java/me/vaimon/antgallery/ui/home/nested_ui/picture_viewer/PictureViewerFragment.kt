dd5730bcefb908a1bfd8207c672c2acdeb9ae795
Andrew Nikolaev <Andrew.Vaimon@yandex.ru>
2024-02-22 10:31:47 +0300

    Fix tablayout

--- app/src/main/java/me/vaimon/antgallery/ui/home/nested_ui/picture_viewer/PictureViewerFragment.kt
+++ app/src/main/java/me/vaimon/antgallery/ui/home/nested_ui/picture_viewer/PictureViewerFragment.kt
@@ -6,13 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayoutMediator
+import me.vaimon.antgallery.R
 import me.vaimon.antgallery.databinding.FragmentPictureViewerBinding
 import me.vaimon.antgallery.ui.home.nested_ui.picture_viewer.adapters.PictureViewerTabAdapter
 
 class PictureViewerFragment : Fragment() {
     private lateinit var binding: FragmentPictureViewerBinding
 
-    private val tabAdapter by lazy {PictureViewerTabAdapter(this)}
+    private var tabAdapter: PictureViewerTabAdapter? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,13 +24,24 @@ class PictureViewerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupAdapters()
         setupTabLayout()
+    }
+
+    private fun setupAdapters() {
+        tabAdapter = PictureViewerTabAdapter(this)
     }
 
     private fun setupTabLayout(){
         binding.viewPager.apply {
             adapter = tabAdapter
-            TabLayoutMediator(binding.tabLayout,this) { _, _ -> }.attach()
+            TabLayoutMediator(binding.tabLayout,this) { tab, position ->
+                tab.text = when(position){
+                    0 -> getString(R.string.tab_new)
+                    1 -> getString(R.string.tab_popular)
+                    else -> throw IllegalStateException("There must be only 2 tabs!")
+                }
+            }.attach()
         }
 
     }
